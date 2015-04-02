bb1303520c4bf083c45d5bef9bae82314a19d5b9
Lily Riopelle <lilykriopelle@gmail.com>
2015-04-02 17:18:27 -0400

    Refactor and designate private methods

--- board.rb
+++ board.rb
@@ -8,54 +8,12 @@ class Board
     place_pieces if set_up
   end
 
-  def place_pieces
-    (0..2).each do |row|
-      alternate_pieces(row, :white)
-    end
-
-    (5..7).each do |row|
-      alternate_pieces(row, :black)
-    end
-  end
-
-  def alternate_pieces(row, color)
-    (0..7).each do |col|
-      if (row.even? && col.odd?) || (row.odd? && col.even?)
-        Checker.new(self, color, [row,col])
-      end
-    end
-  end
-
-  def [] (pos)
-    @grid[pos.first][pos.last]
-  end
-
-  def []=(pos, piece)
-    @grid[pos.first][pos.last] = piece
-  end
-
-  def display
-    puts render
-  end
-
-  def pieces(color)
-    @grid.flatten.compact.select{|piece| piece.color == color}
-  end
-
   def win?(color)
     @grid.flatten.compact.none?{|piece| piece.color == other_color(color)}
   end
 
-  def other_color(color)
-    color == :white ? :black : :white
-  end
-
-  def inspect
-    render
-  end
-
-  def in_bounds?(pos)
-    pos.first.between?(0,7) && pos.last.between?(0,7)
+  def pieces(color)
+    @grid.flatten.compact.select{|piece| piece.color == color}
   end
 
   def dup
@@ -67,25 +25,63 @@ class Board
     new_board
   end
 
-  def render
-     background = :gray
-     nums = ("1".."8").to_a
+  def display
+    puts render
+  end
 
-     "   " + ('a'..'h').to_a.join("  ") + "\n" +
-     @grid.map do |row|
-       background == :white ? background = :gray : background = :white
+  def [] (pos)
+    @grid[pos.first][pos.last]
+  end
 
-       (nums.shift + " ") + row.map do |piece|
+  def []=(pos, piece)
+    @grid[pos.first][pos.last] = piece
+  end
+
+  def in_bounds?(pos)
+    pos.first.between?(0,7) && pos.last.between?(0,7)
+  end
+
+  private
+    def place_pieces
+      (0..2).each do |row|
+        alternate_pieces(row, :white)
+      end
+
+      (5..7).each do |row|
+        alternate_pieces(row, :black)
+      end
+    end
+
+    def alternate_pieces(row, color)
+      (0..7).each do |col|
+        if (row.even? && col.odd?) || (row.odd? && col.even?)
+          Checker.new(self, color, [row,col])
+        end
+      end
+    end
+
+    def other_color(color)
+      color == :white ? :black : :white
+    end
+
+    def render
+       background = :gray
+       nums = ("1".."8").to_a
+
+       "   " + ('a'..'h').to_a.join("  ") + "\n" +
+       @grid.map do |row|
          background == :white ? background = :gray : background = :white
 
-         if piece.nil?
-           ("   ").colorize(:background => background)
-         else
-           (' ' + piece.render + ' ').colorize(:background => background)
-         end
+         (nums.shift + " ") + row.map do |piece|
+           background == :white ? background = :gray : background = :white
 
-       end.join("")
-     end.join("\n")
-   end
+           if piece.nil?
+             ("   ").colorize(:background => background)
+           else
+             (' ' + piece.render + ' ').colorize(:background => background)
+           end
 
+         end.join("")
+       end.join("\n")
+     end
 end
--- checker.rb
+++ checker.rb
@@ -32,153 +32,153 @@ class Checker
     end
   end
 
-  def move_error(sequence)
-    piece_clone = board.dup[position]
-    begin
-      piece_clone.perform_moves!(sequence)
-    rescue InvalidMoveError => e
-      e
-    rescue ForcedJumpError => e
-      e
-    else
-      nil
-    end
-  end
-
-  def perform_moves!(sequence)
-    sequence = sequence.dup
-    num_moves = sequence.size
-    target = sequence.shift
-
-    if board.pieces(color).any? {|piece| piece.valid_jumps.count > 0}
-      raise ForcedJumpError.new "Forced to jump." if valid_slides.include?(target)
-      perform_jump(target)
-    else
-      if num_moves == 1
-        unless (perform_slide(target) || perform_jump(target))
-          raise InvalidMoveError.new "Invalid move."
-        end
-      else
-        until target.nil?
-          raise InvalidMoveError.new "Invalid move." unless perform_jump(target)
-          target = sequence.shift
-        end
-      end
-    end
-  end
-
-  def perform_slide(target)
-    is_valid_move = valid_slides.include?(target)
-    if is_valid_move
-      report_new_pos_to_board(target)
-    end
-    maybe_promote
-
-    is_valid_move
-  end
-
-  def perform_jump(target)
-    is_valid_move = valid_jumps.include?(target)
-    if is_valid_move
-      remove_jumped_piece(target)
-      report_new_pos_to_board(target)
-    end
-    maybe_promote
-
-    is_valid_move
-  end
-
-  def king?
-    @king
-  end
-
-  def remove_jumped_piece(jump)
-    self.board[[row + (jump.first - row)/2, col + (jump.last - col)/2]] = nil
-  end
-
-  def report_new_pos_to_board(new_pos)
-    old_pos = position
-    self.position = new_pos
-    self.board[old_pos] = nil
-    self.board[position] = self
-  end
-
-  def valid_slides
-    diffs = get_slide_diffs
-    diffs.map{ |d_row, d_col| [row + d_row, col + d_col] }
-         .select{|new_pos| board.in_bounds?(new_pos)}
-         .reject{|new_pos| !board[new_pos].nil?}
-  end
-
-  def valid_jumps
-    reachable_jumps.select{|jump| board[jump].nil?}
-  end
-
-  require 'byebug'
-  def reachable_jumps
-    diffs = get_jump_diffs
-    diffs.reject{|dir| adjacent_square_not_occupied_by_enemy(dir) }
-         .map { |(d_row, d_col)| [row+d_row, col+d_col] }
-         .select {|new_pos| board.in_bounds?(new_pos)}
-  end
-
-  def adjacent_square_not_occupied_by_enemy(dir)
-    return false unless board.in_bounds?([row + dir.first, col + dir.last])
-    board[adjacent_square(dir)].nil? || !enemy?(board[adjacent_square(dir)])
-  end
-
-  def adjacent_square(dir)
-    [row + (dir.first / 2), col + (dir.last / 2)]
-  end
-
-  def get_jump_diffs
-    if king?
-      diffs = JUMPS_DOWN + JUMPS_UP
-    else
-      diffs = (color == :white ? JUMPS_DOWN : JUMPS_UP)
-    end
-  end
-
-  def get_slide_diffs
-    if king?
-      diffs = SLIDES_DOWN + SLIDES_UP
-    else
-      diffs = (color == :white ? SLIDES_DOWN : SLIDES_UP)
-    end
-  end
-
-  # TO DO: render kings
   def render
     symbols[color]
   end
 
-  def symbols
-    if king?
-      {white: '☆', black: '★'}
-    else
-      {white: '◎', black: '◉'}
+  protected
+    def perform_moves!(sequence)
+      sequence = sequence.dup
+      num_moves = sequence.size
+      target = sequence.shift
+
+      if board.pieces(color).any? {|piece| piece.valid_jumps.count > 0}
+        raise ForcedJumpError.new "Forced to jump." if valid_slides.include?(target)
+        perform_jump(target)
+      else
+        if num_moves == 1
+          unless (perform_slide(target) || perform_jump(target))
+            raise InvalidMoveError.new "Invalid move."
+          end
+        else
+          until target.nil?
+            raise InvalidMoveError.new "Invalid move." unless perform_jump(target)
+            target = sequence.shift
+          end
+        end
+      end
     end
-  end
 
-  def enemy?(other)
-    color != other.color
-  end
-
-  def maybe_promote
-    if at_last_row?
-      @king = true
+    def valid_slides
+      diffs = get_slide_diffs
+      diffs.map{ |d_row, d_col| [row + d_row, col + d_col] }
+           .select{|new_pos| board.in_bounds?(new_pos)}
+           .reject{|new_pos| !board[new_pos].nil?}
     end
-  end
 
-  def at_last_row?
-    color == :white && row == 7 || color == :black && row == 0
-  end
+    def valid_jumps
+      reachable_jumps.select{|jump| board[jump].nil?}
+    end
 
-  def row
-    position.first
-  end
+  private
+    def perform_slide(target)
+      is_valid_move = valid_slides.include?(target)
+      if is_valid_move
+        report_new_pos_to_board(target)
+      end
+      maybe_promote
 
-  def col
-    position.last
-  end
+      is_valid_move
+    end
+
+    def perform_jump(target)
+      is_valid_move = valid_jumps.include?(target)
+      if is_valid_move
+        remove_jumped_piece(target)
+        report_new_pos_to_board(target)
+      end
+      maybe_promote
+
+      is_valid_move
+    end
+
+    def remove_jumped_piece(jump)
+      self.board[[row + (jump.first - row)/2, col + (jump.last - col)/2]] = nil
+    end
+
+    def king?
+      @king
+    end
+
+    def report_new_pos_to_board(new_pos)
+      old_pos = position
+      self.position = new_pos
+      self.board[old_pos] = nil
+      self.board[position] = self
+    end
+
+    def reachable_jumps
+      diffs = get_jump_diffs
+      diffs.reject{|dir| adjacent_square_not_occupied_by_enemy(dir) }
+           .map { |(d_row, d_col)| [row+d_row, col+d_col] }
+           .select {|new_pos| board.in_bounds?(new_pos)}
+    end
+
+    def adjacent_square_not_occupied_by_enemy(dir)
+      return false unless board.in_bounds?([row + dir.first, col + dir.last])
+      board[adjacent_square(dir)].nil? || !enemy?(board[adjacent_square(dir)])
+    end
+
+    def adjacent_square(dir)
+      [row + (dir.first / 2), col + (dir.last / 2)]
+    end
+
+    def get_jump_diffs
+      if king?
+        diffs = JUMPS_DOWN + JUMPS_UP
+      else
+        diffs = (color == :white ? JUMPS_DOWN : JUMPS_UP)
+      end
+    end
+
+    def get_slide_diffs
+      if king?
+        diffs = SLIDES_DOWN + SLIDES_UP
+      else
+        diffs = (color == :white ? SLIDES_DOWN : SLIDES_UP)
+      end
+    end
+
+    def symbols
+      if king?
+        {white: '☆', black: '★'}
+      else
+        {white: '◎', black: '◉'}
+      end
+    end
+
+    def enemy?(other)
+      color != other.color
+    end
+
+    def maybe_promote
+      if at_last_row?
+        @king = true
+      end
+    end
+
+    def move_error(sequence)
+      piece_clone = board.dup[position]
+      begin
+        piece_clone.perform_moves!(sequence)
+      rescue InvalidMoveError => e
+        e
+      rescue ForcedJumpError => e
+        e
+      else
+        nil
+      end
+    end
+
+    def at_last_row?
+      color == :white && row == 7 || color == :black && row == 0
+    end
+
+    def row
+      position.first
+    end
+
+    def col
+      position.last
+    end
 end
