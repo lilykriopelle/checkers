a207362adcc4611ee9f00f898359e74d21d3810a
App Academy Student <appacademy@Apps-Mac-mini-39.local>
2015-04-02 10:25:06 -0400

    Return reachable jumps + slides

--- checker.rb
+++ checker.rb
@@ -1,4 +1,10 @@
 class Checker
+  attr_accessor :color, :position, :board, :king
+
+  SLIDES_DOWN = [[1,-1], [1,1]]
+  SLIDES_UP =   [[-1,-1], [-1,1]]
+  JUMPS_DOWN =  [[2,-2], [2,2]]
+  JUMPS_UP =    [[-2,-2], [-2,2]]
 
   def initialize(board, color, position, king = false)
     @board = board
@@ -7,18 +13,48 @@ class Checker
     @king = king
   end
 
-  private
+  def slide(target)
 
-    def slide(target)
-      #return true if sliding to target is valid, false otherwise
-    end
+    #return true if sliding to target is valid, false otherwise
+  end
 
-    def jump(target)
-      #return true if sliding to target is valid, false otherwise
-    end
+  def jump(target)
+    #return true if sliding to target is valid, false otherwise
+  end
 
-    def maybe_promote
 
-    end
+  # MOVE TO BOARD
+  def in_bounds(pos)
+    pos.first.between?(0,7) && pos.last.between?(0,7)
+  end
+
+  def slides
+    diffs = (color == :white ? SLIDES_DOWN : SLIDES_UP)
+    diffs.map { |d_row, d_col| [row+d_row, col+d_col] }
+         .select {|new_pos| in_bounds(new_pos)}
+  end
+
+  # reject jumps if space one away isn't occupied
+  # reject jumping onto occupied spaces
+  def jumps
+    diffs = (color == :white ? JUMPS_DOWN : JUMPS_UP)
+    diffs.map { |d_row, d_col| [row+d_row, col+d_col] }
+         .select {|new_pos| in_bounds(new_pos)}
+  end
+
+  def occupied?(pos)
+  end
+
+  def maybe_promote
+
+  end
+
+  def row
+    position.first
+  end
+
+  def col
+    position.last
+  end
 
 end
